63154f05fe48e7e7d83949a1c37cdc9b6336ee6b
Miguel Angel Ramirez Lopez <C-MiguelAngel.Ramirez@charter.com>
2024-10-10 19:08:45 -0500

    avance curso

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+// import {HelloWorldScreen} from './src/presentation/screens/HelloWorldScreen';
+import {CounterScreen} from './src/presentation/screens/CounterScreen';
 
 export const App = () => {
   return (
-    <View>
-      <Text>Hola Mundo</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{flex: 1}}>
+      {/* <HelloWorldScreen name="Miguel Angel" /> */}
+      <CounterScreen />
+    </SafeAreaView>
+  );
+};
